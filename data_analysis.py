2e3bc90273a8db2d8656d5b3cfa2ef231992de0a
Dave Babbitt <dave.babbitt@gmail.com>
2024-12-27 18:11:09 -0500

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -2078,7 +2078,7 @@ class DataAnalysis(BaseConfig):
             122, projection="3d"
         )  # 1 row, 2 columns, 2nd subplot
         ax2.scatter(
-            spread_points[:, 0], spread_points[:, 1], spread_points[:, 2],
+            spread_points[1:, 0], spread_points[1:, 1], spread_points[1:, 2],
             c=colors, s=100, edgecolors=fixed_point, linewidth=3,
             label='Spread Points', alpha=1.0
         )
